d22ee1c44b5ff3f39e4ff95e486a605847927b46
saktiajadeh <saktiajadeh29@gmail.com>
2021-01-19 22:04:53 +0800

    update route web

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::resource('/kriteria', 'kriteriaController');
+Route::resource('/kriteria', KriteriaController::class);
